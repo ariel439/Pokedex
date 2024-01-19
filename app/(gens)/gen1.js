62b077ca554e4a704f4e35dfc9526c444ee62327
ariel439 <dolzanariel@gmail.com>
2024-01-19 04:16:56 -0300

    updated

---
+++ app/(gens)/gen1.js
@@ -0,0 +1,8 @@
+import Gen from "../components/gen"
+
+export default function Gen1() {
+
+    return(
+        <Gen pokeStart={1} pokeEnd={151} genNumber={1}/>
+    )
+}
--- app/home.js
+++ app/home.js
@@ -1,61 +1,36 @@
-import { useEffect, useState } from "react";
-import { Text, View, FlatList, Image, TouchableOpacity } from "react-native";
-import axios from 'axios';
-import { styles } from "../util/style";
-import { getTypeColor } from "../util/functions";
-import Icon from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { router } from "expo-router";
 
 export default function Home() {
 
-    //useState
-    const [pokemons, setPokemons] = useState([]);
-
-    //useEffect
-    useEffect(() => {
-        getPokemon();
-    }, []);
-
-    //get pokemons from api
-    const getPokemon = () => {
-        var endpoints = []
-        for ( var i = 1;  i <= 151; i++){
-            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-        }
-        axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
-    }
-
-    const renderPokemon = ({item}) => {
-        return(
-            <TouchableOpacity style={styles.dex} activeOpacity={1} onPress={() => router.push("/pokes/" + item.data.id)}>
-                <View style={styles.dexInfo}>
-                    <Text style={{fontSize: 20, paddingTop: 5}}>{item.data.name.charAt(0).toUpperCase() + item.data.name.slice(1)}</Text>
-                    <View style={styles.types}>
-                        {item.data.types.map((t, index) =>
-                            <View style={[styles.type, {backgroundColor:getTypeColor(t.type.name)}]} key={index}>
-                                <Text>{t.type.name}</Text>
-                            </View>
-                        )}
-                    </View>
-                </View>
-                <Image
-                style={styles.img}
-                source={{uri: item.data.sprites.front_default}}
-                />
-            </TouchableOpacity>
-        )
-    }
 
     return(
         <View style={styles.container}>
-            <View style={styles.top}>
-                <Text style={[styles.title, {paddingLeft: 35}]}>Generation 1</Text>
-                <Icon name="arrow-forward-outline" size={35} onPress={() => router.push("gen2")}></Icon>
-            </View>
-            <FlatList
-                data={pokemons} 
-                renderItem={renderPokemon}
-            />
+            <TouchableOpacity style={styles.button} onPress={() => router.push("gen1")}><Text>Generation 1</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("gen2")}><Text>Generation 2</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("gen3")}><Text>Generation 3</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("gen4")}><Text>Generation 4</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("gen5")}><Text>Generation 5</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("gen6")}><Text>Generation 6</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("gen7")}><Text>Generation 7</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("gen8")}><Text>Generation 8</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("gen9")}><Text>Generation 9</Text></TouchableOpacity>
         </View>
     )
 }
+
+export const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-evenly"
+    },
+    button: {
+        width: "50%",
+        height: 60,
+        backgroundColor: "grey",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 20,
+    }
+})
